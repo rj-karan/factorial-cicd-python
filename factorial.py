10fe9d6f1c00d605f3233cdb7cf3681af724e16f
zoro <rjkaran418@gmail.com>
2026-09-24 09:31:08 +0530

    Fix factorial implementation and CI dependencies

--- factorial.py
+++ factorial.py
@@ -1,7 +1,9 @@
 def factorial(n):
-	if n<0:
-		raise ValueError("Factorial is not defined for negative numbers")
-	r=1
-	for i in range(1,n+1):
-		r*=1
-	return r
+    if n < 0:
+        raise ValueError("Factorial is not defined for negative numbers")
+
+    r = 1
+    for i in range(1, n + 1):
+        r *= i
+
+    return r
